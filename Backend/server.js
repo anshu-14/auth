86ce75bc46c1fc27ee724003cab13720498cfc36
anshu-14 <vishwanshu152000@gmail.com>
2025-05-31 19:32:45 +0530

    added cors in backend, login and signup and auth context

--- Backend/server.js
+++ Backend/server.js
@@ -2,13 +2,18 @@ const express = require('express');
 const dotenv = require('dotenv');
 const allRoutes = require('./routes/index')
 const connectDB = require('./config/db');//importing the db connection file
-
+const cors = require('cors');
 
 
 
 dotenv.config();//adds .env to process and can be used throughout the project using process.env
 
 const app = express();//create insatance of express 
+// Allow requests from your frontend
+app.use(cors({
+  origin: '*', // or use "*" for all
+  credentials: true // if you plan to use cookies or sessions
+}));
 app.use(express.json());//parse the json that we passed and attach it in req.body
 connectDB();//connect to the database
 app.get('/', (req, res) => res.send('API is running...'));
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,9 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import 'primereact/resources/themes/lara-dark-indigo/theme.css'// theme
+import 'primereact/resources/primereact.min.css'                 // Core CSS
+import 'primeicons/primeicons.css'                               // Icons
+import 'primeflex/primeflex.css'  
+import { PrimeReactProvider } from 'primereact/api'
+import { AuthProvider } from './auth/AuthContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <PrimeReactProvider value={{// here primereactprovider sets global config for the project like input filed style and we can also set local setting
+      ripple: true,
+      inputStyle: 'filled' 
+    }}>
+      <AuthProvider>
+
     <App />
+      </AuthProvider>
+    </PrimeReactProvider>
+  
   </StrictMode>,
 )
